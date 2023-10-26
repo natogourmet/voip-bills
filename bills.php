bc0fa1a19d5f3aed8a1784bddcdb0aac55a46eef
stiveng <natogourmet@hotmail.com>
2023-10-26 16:56:18 -0500

    first try

---
+++ bills.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php -q
+<?php
+require('phpagi.php');
+$agi = new AGI();
+$agi->answer();
+sleep(1);
+$agi->exec_agi("googletts.agi,\"This is a DB connection test\",en");
+
+// ENV VAR SETTING
+require("definiciones.inc");
+$link = mysql_connect(HOST, USER, PWD);
+mysql_select_db(DB, $link);
+
+// RECEIVING ID INPUT
+$agi->exec_agi("googletts.agi,\"Please, type your ID number followed by the Sharp Sign\",en");
+$num = $agi->get_data('beep', 3000, 20, '#');
+$userId = $num['result'];
+
+// VALIDATING USER ID INPUT
+if ($userId == '') {
+	$agi->exec_agi("googletts.agi,\"You didn't input any ID\",en");
+	$agi->hangup();
+}
+
+// RETRIEVING BILL VALUE FROM DB
+$query = 'SELECT * FROM bills WHERE user_id=' . $userId;
+$result = mysql_query($query, $link);
+$row = mysql_fetch_array($result);
+
+$debt = $row['bill_value'] - $row['balance'];
+$agi->exec_agi("googletts.agi,\"Your pending balance is " . $debt . " Colombian Pesos\",en");
+?>
---
+++ privileges.sql
@@ -0,0 +1,2 @@
+grant all privileges on Bills.* to 'root'@'localhost' identified by 'root' with grant option;
+flush privileges;
